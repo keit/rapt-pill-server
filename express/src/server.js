cfadb7fc6dcb630939f5f2ef5f9f1251dc93bc74
Kei Takashima <kei.takashima@crssoftware.co.nz>
2024-10-08 13:35:53 +1300

    use Docker

---
+++ express/src/server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const next = require("next");
+
+const dev = process.env.NODE_ENV !== "production";
+const nextApp = next({ dev });
+const handle = nextApp.getRequestHandler();
+
+const app = express();
+// Middleware to parse JSON body
+app.use(express.json());
+
+// Prepare Next.js
+nextApp.prepare().then(() => {
+  // Define any API routes first
+  app.post("/pillData", (req, res) => {
+    const currentTime = new Date();
+    console.log(currentTime.toLocaleTimeString());
+    console.log(req.body);
+    // Handle your API endpoint logic here (e.g., receiving data from Arduino)
+    res.json({});
+  });
+
+  // Serve Next.js pages
+  app.all("*", (req, res) => {
+    return handle(req, res);
+  });
+
+  app.listen(3000, (err) => {
+    if (err) throw err;
+    console.log("Server ready on http://localhost:3000");
+  });
+});
